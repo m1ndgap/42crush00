cb7adad0abbf7bb219db2fa38380b1a0b70ac46a
Tim Chumbash <tchumbas@c2r8s8.42lisboa.com>
2025-08-03 15:14:27 +0100

    rush01.c wip

--- rush01.c
+++ rush01.c
@@ -6,7 +6,7 @@
 /*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/03 14:33:09 by tchumbas          #+#    #+#             */
-/*   Updated: 2025/08/03 14:33:47 by tchumbas         ###   ########.fr       */
+/*   Updated: 2025/08/03 15:12:47 by tchumbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,32 @@
 
 void	ft_putchar();
 
-void	edge_line(int x)
+char corner_direction(int x, int ox, int y)
+{
+	if ((x - ox) == 0 || y > 1) 
+	{
+		return '/';
+	}
+	else if ( x == 1 ) 
+	{
+		return '\\'; 
+	}
+	else if ((x - ox) == 0)
+	{
+		return 'X';
+	}
+	return 'Y';
+}
+
+void	edge_line(int x, int y)
 {
 	int startX = x;
 
 	while (x > 0) {
 		if ((x - startX) == 0 || (x == 1))
 		{
-			ft_putchar('/');
+			char corner = corner_direction(x, startX, y);
+			ft_putchar(corner);
 			x--;
 		} 
 		else if (x > 1)
@@ -49,7 +67,7 @@ void	gap_line(int x, int y)
 {
 	int gap = x - 2;
 
-	if (gap <= 0) {
+	if (gap < 0) {
 		ft_putchar('*');
 		ft_putchar('\n');
 	} 
@@ -70,18 +88,19 @@ void	rush(int x, int y)
 {
 	if (y <= 0 || x <= 0)
 	{
+		char msg[] = "ERROR: negative value provided\n";
+    	write(2, msg, sizeof(msg) - 1);
 		return;
 	}
-	edge_line(x);
+	edge_line(x, y);
 	while ((y - 2) > 0)
 	{
 		gap_line(x, y);
 		y--;
 	}
 	if (y > 1) {
-		edge_line(x);
+		edge_line(x, y);
 	}
-	
-	
+		
 	ft_putchar('\n');	
 }
