169bb1615caec1a25dec9959e8f703f3d4365419
Tim Chumbash <tchumbas@c2r8s8.42lisboa.com>
2025-08-03 14:34:23 +0100

    starting rush01

---
+++ rush01.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 14:33:09 by tchumbas          #+#    #+#             */
+/*   Updated: 2025/08/03 14:33:47 by tchumbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchumbas <tchumbas@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 14:26:55 by tchumbas          #+#    #+#             */
+/*   Updated: 2025/08/02 16:23:33 by tchumbas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar();
+
+void	edge_line(int x)
+{
+	int startX = x;
+
+	while (x > 0) {
+		if ((x - startX) == 0 || (x == 1))
+		{
+			ft_putchar('/');
+			x--;
+		} 
+		else if (x > 1)
+		{
+			ft_putchar('*');
+			x--;
+		}
+	}
+	ft_putchar('\n');
+}
+
+void	gap_line(int x, int y)
+{
+	int gap = x - 2;
+
+	if (gap <= 0) {
+		ft_putchar('*');
+		ft_putchar('\n');
+	} 
+	else if (y > 0) 
+	{
+		ft_putchar('*');
+		while(gap > 0) {
+			ft_putchar(' ');
+			gap--;
+		}
+		ft_putchar('*');
+		ft_putchar('\n');
+	}
+}
+
+
+void	rush(int x, int y)
+{
+	if (y <= 0 || x <= 0)
+	{
+		return;
+	}
+	edge_line(x);
+	while ((y - 2) > 0)
+	{
+		gap_line(x, y);
+		y--;
+	}
+	if (y > 1) {
+		edge_line(x);
+	}
+	
+	
+	ft_putchar('\n');	
+}
